542e20427fa99aea6ab2d804a828fca2a760b48f
alsiough <alexit200543@gmail.com>
2024-10-13 19:30:26 +0300

    lab 1.5

--- labs/1.5/main.cpp
+++ labs/1.5/main.cpp
@@ -1,7 +1,7 @@
 /**
  * Done by:
- * Student Name: Severyn Kotyhoroshko
- * Student Group: 123
+ * Student Name: Serhiienko Oleksandr
+ * Student Group: 121
  * Lab 1.5
  */
 
@@ -11,31 +11,24 @@
 using namespace std;
 
 int main() {
-    {
-        int a = 10;
-        int b = 20;
-        int c = 30;
-        int d = 40;
 
-        // <УЛО1> (<УЛО2> (A<ОВ1>B) <БЛО> (<УЛО3> (C<ОВ2>D)))
-        //  empty (  !    (a == b)    ^   (   !   (c != d)))
-        bool res = (!(a == b) ^ (!(c != d)));
-        cout << "res: " << boolalpha << res << endl;
-    }
-     // 010101
-     // 101011
+    const int A = 17;
+    const int B = -23;
+    const int C = 33;
+    const int D = 2;
+    const int E = 26054;
+    const double F = sizeof(double);
 
-    {
-        int a = 10;
-        int b = 20;
-        int c = 30;
-        int d = 40;
 
-        // <УЛО1> (<УЛО2> (A<ОВ1>B) <БЛО> (<УЛО3> (C<ОВ2>D)))
-        //  empty (  !    (a == b)    and   (   !   (c != d)))
-        bool res = (!(a == b) and (!(c != d)));
-        cout << "res: " << boolalpha << res << endl;
-    }
+    int R = (A ^ B) * (C >> D) * (~E);
+
+
+    double L = (R == F) ? R : F;
+
+
+    cout << "R = " << R << endl;
+    cout << "L = " << L << endl;
 
     return 0;
 }
+
